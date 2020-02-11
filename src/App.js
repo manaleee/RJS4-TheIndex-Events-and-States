ad960b7419f1aeb182b1fc65e3960ff84b240c5b
Manal Alkandari <manal_alkandri@hotmail.com>
2020-02-11 20:58:40 +0300

    task04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Data
 import authors from "./data";
@@ -6,20 +6,38 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
+import AuthorDetail from "./AuthorDetail";
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorList authors={authors} />
+class App extends Component {
+  state = {
+    currentAuthor: null
+  };
+  selectAuthor = author => {
+    this.setState({ currentAuthor: author });
+  };
+
+  handleToggle = () => {
+    if (this.state.currentAuthor)
+      return <AuthorDetail author={this.state.currentAuthor} />;
+    return <AuthorList authors={authors} selectAuthor={this.selectAuthor} />;
+  };
+
+  nullAuthor = () => {
+    this.setState({ currentAuthor: null });
+  };
+
+  render() {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar nullAuthor={this.nullAuthor} />
+          </div>
+          <div className="content col-10">{this.handleToggle()}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/AuthorList.js
+++ src/AuthorList.js
@@ -2,15 +2,27 @@ import React from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
+import SearchBar from "./SearchBar";
 
 const AuthorList = props => {
   const authorCards = props.authors.map(author => (
-    <AuthorCard key={author.first_name + author.last_name} author={author} />
+    <AuthorCard
+      key={author.first_name + author.last_name}
+      author={author}
+      selectAuthor={props.selectAuthor}
+    />
   ));
 
+  const filterAuthors = query => {
+    const filteredAuthors = props.authors.filter(author =>
+      `${author.first_name} ${author.last_name}`.includes(query)
+    );
+    console.log(filteredAuthors);
+  };
   return (
     <div className="authors">
       <h3>Authors</h3>
+      <SearchBar filterAuthors={filterAuthors} />
       <div className="row">{authorCards}</div>
     </div>
   );
